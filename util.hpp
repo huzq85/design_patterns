30592d4efc41481a32a0841698ddeeac5e3f50bd
Zhenqing Hu <huzq85@icloud.com>
2020-07-10 19:52:37 -0500

    To add util file.

---
+++ util.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+// void DEBUG(std::string debug_info)
+// {
+//     std::cout << "DEBUG: " << debug_info << std::endl;
+// }
+
+#define DEBUG(x) std::cout<<x
